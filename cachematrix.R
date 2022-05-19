e6ec496ca670d3f596c13a54b9792228be3cebcf
fake-skymall <mgcsipo@gmail.com>
2022-05-19 00:58:26 -0400

    Submission for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-##create NULL matrix m of matching x dim
+##Learning how to cache a matrix & assign to the parent environment
 ##set the value of the matrix
 ##get the value of the matrix
 ##set the value of the inv
 ##get the value of the inv
 
 makeCacheMatrix <- function(x = matrix()) {
-  #dim<-dim(x)
-  #m<-matrix(,xdim[1],xdim[2])
+
   m<-NULL
   set <- function(y) {
     x <<- y
@@ -26,7 +22,6 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Populate or retrieve inverse of matrix x
 
 cacheSolve <- function(x, ...) {
-## Return a matrix that is the inverse of 'x'
   
   m <- x$getinv()
   if(!is.null(m)) {
